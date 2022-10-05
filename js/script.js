3747df5b2573d1aa23d7e7db2cdccf4f262e3cb8
santiescobar <santiescobar948@gmail.com>
2022-10-05 13:07:16 -0300

    trabajo practico 6

---
+++ js/script.js
@@ -0,0 +1,41 @@
+//#region 
+var perro1 = "Eva";
+const perro2 = 'Atreus';
+let gato = `Varus`;
+var inicio=0;
+var fin=[3];
+//#endregion
+
+//#region 
+var suma = (num1,num2) =>{
+    console.log(num1+num2);
+}
+//#endregion
+
+//#region 
+var resta = (num1,num2) =>{
+    console.log(num1-num2);
+}
+//#endregion
+
+//#region 
+var multiplicar = (num1,num2) =>{
+    console.log(num1*num2);
+}
+//#endregion
+
+//#region 
+var division = (num1,num2) =>{
+    console.log(num1/num2);
+}
+//#endregion
+
+//#region 
+let animales=[perro1,perro2,gato];
+//#endregion
+
+//#region 
+for(incio of fin){
+    console.log(animales[inicio]);
+}
+//#endregion
